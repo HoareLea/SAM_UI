0bbb64c1202dfebce158ceac57d3852e88340e53
Michal Dengusiak <tassimulation@gmail.com>
2022-04-06 10:41:06 +0100

    WIP intallation .exe

--- SAM_UI/SAM.Core.UI/Properties/Resources.Designer.cs
+++ SAM_UI/SAM.Core.UI/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace SAM.Weather.UI.Properties {
+namespace SAM.Core.UI.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace SAM.Weather.UI.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SAM.Weather.UI.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SAM.Core.UI.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
