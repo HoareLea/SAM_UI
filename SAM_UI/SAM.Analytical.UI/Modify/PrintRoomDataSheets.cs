5f12bb4a3ef6ed753f04502d9e76f59f4e2c3e9b
Ziolkowski <jakubziolkowski@digiproject.uk>
2022-05-17 11:02:54 +0100

    PrintRoomDataSheet Missing Results Added

--- SAM_UI/SAM.Analytical.UI/Modify/PrintRoomDataSheets.cs
+++ SAM_UI/SAM.Analytical.UI/Modify/PrintRoomDataSheets.cs
@@ -74,6 +74,8 @@ namespace SAM.Analytical.UI
                 return;
             }
 
+            int year = 2018;
+
             string path = System.IO.Path.Combine(directory, System.IO.Path.GetFileName(path_Template));
 
             System.IO.File.Copy(path_Template, path, true);
@@ -133,6 +135,49 @@ namespace SAM.Analytical.UI
                         values[i, 27] = ventilationRiserName;
                     }
 
+                    if (space.TryGetValue(SpaceParameter.SupplyAirFlow, out double supplyAirFlow) && !double.IsNaN(supplyAirFlow))
+                    {
+                        values[i, 40] = supplyAirFlow;
+                    }
+
+                    if (space.TryGetValue(SpaceParameter.ExhaustAirFlow, out double exhaustAirFlow) && !double.IsNaN(exhaustAirFlow))
+                    {
+                        values[i, 41] = exhaustAirFlow;
+                    }
+
+                    if (space.TryGetValue(SpaceParameter.OutsideSupplyAirFlow, out double outsideSupplyAirFlow) && !double.IsNaN(outsideSupplyAirFlow))
+                    {
+                        values[i, 42] = outsideSupplyAirFlow;
+                    }
+
+                    if (space.TryGetValue(SpaceParameter.DesignHeatingLoad, out double designHeatingLoad) && !double.IsNaN(designHeatingLoad))
+                    {
+                        values[i, 60] = designHeatingLoad;
+                    }
+
+                    if (space.TryGetValue(SpaceParameter.DesignCoolingLoad, out double designCoolingLoad) && !double.IsNaN(designCoolingLoad))
+                    {
+                        values[i, 62] = designCoolingLoad;
+                    }
+
+                    double coolingDesignTemperature = Analytical.Query.CoolingDesignTemperature(space, analyticalModel);
+                    if(double.IsNaN(coolingDesignTemperature))
+                    {
+                        values[i, 67] = coolingDesignTemperature;
+                    }
+
+                    double heatingDesignTemperature = Analytical.Query.HeatingDesignTemperature(space, analyticalModel);
+                    if (double.IsNaN(heatingDesignTemperature))
+                    {
+                        values[i, 68] = heatingDesignTemperature;
+                    }
+
+                    double specificOccupancySensibleGain = space.SpecificOccupancySensibleGain();
+                    if(!double.IsNaN(specificOccupancySensibleGain))
+                    {
+                        values[i, 45] = specificOccupancySensibleGain;
+                    }
+
                     Geometry.Spatial.Shell shell = adjacencyCluster.Shell(space);
                     if (shell != null)
                     {
@@ -180,12 +225,20 @@ namespace SAM.Analytical.UI
                             values[i, 13] = lightingLevel;
                         }
 
-
                         if (internalCondition.TryGetValue(InternalConditionParameter.OccupancyProfileName, out string occupancyProfileName) && !string.IsNullOrEmpty(occupancyProfileName))
                         {
                             values[i, 21] = occupancyProfileName;
                         }
 
+                        if (internalCondition.TryGetValue(InternalConditionParameter.EquipmentSensibleGainPerArea, out double equipmentSensibleGainPerArea) && !double.IsNaN(equipmentSensibleGainPerArea))
+                        {
+                            values[i, 44] = equipmentSensibleGainPerArea;
+                        }
+
+                        if (internalCondition.TryGetValue(InternalConditionParameter.LightingGainPerArea, out double lightingGainPerArea) && !double.IsNaN(lightingGainPerArea))
+                        {
+                            values[i, 46] = lightingGainPerArea;
+                        }
                     }
 
                     HeatingSystem heatingSystem = Analytical.Query.Systems<HeatingSystem>(adjacencyCluster, space).FirstOrDefault();
@@ -233,6 +286,57 @@ namespace SAM.Analytical.UI
                                 values[i, 19] = volume;
                             }
 
+                            double specificDesignLoad = Analytical.Query.SpecificDesignLoad(spaceSimulationResult_Heating);
+                            if(!double.IsNaN(specificDesignLoad))
+                            {
+                                values[i, 48] = specificDesignLoad;
+                            }
+
+                            if (spaceSimulationResult_Heating.TryGetValue(SpaceSimulationResultParameter.Load, out double load) && !double.IsNaN(load))
+                            {
+                                values[i, 61] = load;
+                            }
+
+                            if (spaceSimulationResult_Heating.TryGetValue(SpaceSimulationResultParameter.InfiltrationGain, out double infiltrationGain) && !double.IsNaN(infiltrationGain))
+                            {
+                                values[i, 69] = infiltrationGain;
+                            }
+
+                            if (spaceSimulationResult_Heating.TryGetValue(SpaceSimulationResultParameter.AirMovementGain, out double airMovementGain) && !double.IsNaN(airMovementGain))
+                            {
+                                values[i, 70] = airMovementGain;
+                            }
+
+                            double specificBuildingHeatTransferGain = spaceSimulationResult_Heating.SpecificBuildingHeatTransferGain();
+                            if (!double.IsNaN(specificBuildingHeatTransferGain))
+                            {
+                                values[i, 71] = specificBuildingHeatTransferGain;
+                            }
+
+                            if (spaceSimulationResult_Heating.TryGetValue(SpaceSimulationResultParameter.GlazingExternalConduction, out double glazingExternalConduction) && !double.IsNaN(glazingExternalConduction))
+                            {
+                                values[i, 72] = glazingExternalConduction;
+                            }
+
+                            if (spaceSimulationResult_Heating.TryGetValue(SpaceSimulationResultParameter.OpaqueExternalConduction, out double opaqueExternalConduction) && !double.IsNaN(opaqueExternalConduction))
+                            {
+                                values[i, 73] = opaqueExternalConduction;
+                            }
+
+                            if (spaceSimulationResult_Heating.TryGetValue(SpaceSimulationResultParameter.SizingMethod, out string sizingMethod) && !string.IsNullOrWhiteSpace(sizingMethod))
+                            {
+                                values[i, 74] = sizingMethod;
+                            }
+
+                            if (spaceSimulationResult_Heating.TryGetValue(SpaceSimulationResultParameter.DryBulbTempearture, out double dryBulbTempearture) && !double.IsNaN(dryBulbTempearture))
+                            {
+                                values[i, 75] = dryBulbTempearture;
+                            }
+
+                            if (spaceSimulationResult_Heating.TryGetValue(SpaceSimulationResultParameter.ResultantTemperature, out double resultantTemperature) && !double.IsNaN(resultantTemperature))
+                            {
+                                values[i, 76] = resultantTemperature;
+                            }
                         }
 
                         values[i, 22] = spaceSimulationResults.ConvertAll(x => x.DateTime).Max();
@@ -240,11 +344,126 @@ namespace SAM.Analytical.UI
                         SpaceSimulationResult spaceSimulationResult_Cooling = spaceSimulationResults.Find(x => x.HasValue(SpaceSimulationResultParameter.LoadType) && LoadType.Cooling.ToString().Equals(x.GetValue(SpaceSimulationResultParameter.LoadType)));
                         if (spaceSimulationResult_Cooling != null)
                         {
+                            if(spaceSimulationResult_Cooling.TryGetValue(SpaceSimulationResultParameter.LoadIndex, out int loadIndex))
+                            {
+                                values[i, 18] = loadIndex;
+                            }
+
+                            if (spaceSimulationResult_Cooling.TryGetValue(SpaceSimulationResultParameter.RelativeHumidity, out double relativeHumidity))
+                            {
+                                values[i, 23] = loadIndex;
+                            }
+
+                            if (loadIndex != -1)
+                            {
+                                values[i, 30] = Convert.ToDateTime(loadIndex, year);
+                            }
+
+                            if (spaceSimulationResult_Cooling.TryGetValue(SpaceSimulationResultParameter.HumidityRatio, out double humidityRatio))
+                            {
+                                values[i, 31] = humidityRatio;
+                            }
+
                             if (spaceSimulationResult_Cooling.TryGetValue(SpaceSimulationResultParameter.SizingMethod, out string sizingMethod) && !string.IsNullOrWhiteSpace(sizingMethod))
                             {
                                 values[i, 32] = sizingMethod;
                             }
 
+                            double specificBuildingHeatTransferGain = spaceSimulationResult_Cooling.SpecificBuildingHeatTransferGain();
+                            if(!double.IsNaN(specificBuildingHeatTransferGain))
+                            {
+                                values[i, 47] = specificBuildingHeatTransferGain;
+                            }
+
+                            if (spaceSimulationResult_Cooling.TryGetValue(SpaceSimulationResultParameter.SolarGain, out double solarGain) && !double.IsNaN(solarGain))
+                            {
+                                values[i, 49] = solarGain;
+                            }
+
+                            if (spaceSimulationResult_Cooling.TryGetValue(SpaceSimulationResultParameter.LightingGain, out double lightingGain) && !double.IsNaN(lightingGain))
+                            {
+                                values[i, 50] = lightingGain;
+                            }
+
+                            if (spaceSimulationResult_Cooling.TryGetValue(SpaceSimulationResultParameter.InfiltrationGain, out double infiltrationGain) && !double.IsNaN(infiltrationGain))
+                            {
+                                values[i, 51] = infiltrationGain;
+                            }
+
+                            if (spaceSimulationResult_Cooling.TryGetValue(SpaceSimulationResultParameter.AirMovementGain, out double airMovementGain) && !double.IsNaN(airMovementGain))
+                            {
+                                values[i, 52] = airMovementGain;
+                            }
+
+                            if (spaceSimulationResult_Cooling.TryGetValue(SpaceSimulationResultParameter.BuildingHeatTransfer, out double buildingHeatTransfer) && !double.IsNaN(buildingHeatTransfer))
+                            {
+                                values[i, 53] = buildingHeatTransfer;
+                            }
+
+                            if (spaceSimulationResult_Cooling.TryGetValue(SpaceSimulationResultParameter.GlazingExternalConduction, out double glazingExternalConduction) && !double.IsNaN(glazingExternalConduction))
+                            {
+                                values[i, 54] = glazingExternalConduction;
+                            }
+
+                            if (spaceSimulationResult_Cooling.TryGetValue(SpaceSimulationResultParameter.OpaqueExternalConduction, out double opaqueExternalConduction) && !double.IsNaN(opaqueExternalConduction))
+                            {
+                                values[i, 55] = opaqueExternalConduction;
+                            }
+
+                            if (spaceSimulationResult_Cooling.TryGetValue(SpaceSimulationResultParameter.OccupancySensibleGain, out double occupancySensibleGain) && !double.IsNaN(occupancySensibleGain))
+                            {
+                                values[i, 56] = occupancySensibleGain;
+                            }
+
+                            if (spaceSimulationResult_Cooling.TryGetValue(SpaceSimulationResultParameter.EquipmentSensibleGain, out double equipmentSensibleGain) && !double.IsNaN(equipmentSensibleGain))
+                            {
+                                values[i, 57] = equipmentSensibleGain;
+                            }
+
+                            if (spaceSimulationResult_Cooling.TryGetValue(SpaceSimulationResultParameter.EquipmentLatentGain, out double equipmentLatentGain) && !double.IsNaN(equipmentLatentGain))
+                            {
+                                values[i, 58] = equipmentLatentGain;
+                            }
+
+                            if (spaceSimulationResult_Cooling.TryGetValue(SpaceSimulationResultParameter.OccupancyLatentGain, out double occupancyLatentGain) && !double.IsNaN(occupancyLatentGain))
+                            {
+                                values[i, 59] = occupancyLatentGain;
+                            }
+
+                            if (spaceSimulationResult_Cooling.TryGetValue(SpaceSimulationResultParameter.Load, out double load) && !double.IsNaN(load))
+                            {
+                                values[i, 63] = load;
+                            }
+
+                            if (spaceSimulationResult_Cooling.TryGetValue(SpaceSimulationResultParameter.DryBulbTempearture, out double dryBulbTempearture) && !double.IsNaN(dryBulbTempearture))
+                            {
+                                values[i, 64] = dryBulbTempearture;
+                            }
+
+                            if (spaceSimulationResult_Cooling.TryGetValue(SpaceSimulationResultParameter.ResultantTemperature, out double resultantTemperature) && !double.IsNaN(resultantTemperature))
+                            {
+                                values[i, 65] = resultantTemperature;
+                            }
+
+                            if (spaceSimulationResult_Cooling.TryGetValue(SpaceSimulationResultParameter.MaxDryBulbTemperature, out double maxDryBulbTemperature) && !double.IsNaN(maxDryBulbTemperature))
+                            {
+                                values[i, 79] = maxDryBulbTemperature;
+                            }
+
+                            if (spaceSimulationResult_Cooling.TryGetValue(SpaceSimulationResultParameter.MinDryBulbTemperature, out double minDryBulbTemperature) && !double.IsNaN(minDryBulbTemperature))
+                            {
+                                values[i, 80] = minDryBulbTemperature;
+                            }
+
+                            if (spaceSimulationResult_Cooling.TryGetValue(SpaceSimulationResultParameter.MaxDryBulbTemperatureIndex, out int maxDryBulbTemperatureIndex) && !double.IsNaN(maxDryBulbTemperatureIndex))
+                            {
+                                values[i, 81] = Convert.ToDateTime(maxDryBulbTemperatureIndex, year);
+                            }
+
+                            if (spaceSimulationResult_Cooling.TryGetValue(SpaceSimulationResultParameter.MinDryBulbTemperatureIndex, out int minDryBulbTemperatureIndex) && !double.IsNaN(minDryBulbTemperatureIndex))
+                            {
+                                values[i, 82] = Convert.ToDateTime(minDryBulbTemperatureIndex, year);
+                            }
                         }
                     }
                 }
