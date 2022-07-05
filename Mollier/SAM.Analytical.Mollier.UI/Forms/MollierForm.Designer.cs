ad36d1a32cedc9105fbe8f814c26bf0879102959
Ziolkowski <jakubziolkowski@digiproject.uk>
2022-07-05 12:14:11 +0100

    Code Reorganization and Mollier UI Projects Added

---
+++ Mollier/SAM.Analytical.Mollier.UI/Forms/MollierForm.Designer.cs
@@ -0,0 +1,47 @@
+﻿
+namespace SAM.Analytical.Mollier.UI
+{
+    partial class MoilierForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // MoilierForm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "MoilierForm";
+            this.Text = "Mollier";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ Mollier/SAM.Core.Mollier.UI/Forms/MollierForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SAM.Core.Mollier.UI
+{
+    public partial class MollierForm : Form
+    {
+        public MollierForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
