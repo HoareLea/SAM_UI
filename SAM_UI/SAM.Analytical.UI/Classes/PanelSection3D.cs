f5980e77bd574e9cdd00aee73fb46751f8bd5fa5
Ziolkowski <jakubziolkowski@digiproject.uk>
2022-10-25 16:43:42 +0100

    UIGeometryObjectModel Initail Implementation

---
+++ SAM_UI/SAM.Analytical.UI/Classes/PanelSection3D.cs
@@ -0,0 +1,23 @@
+﻿using SAM.Geometry.Spatial;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAM.Analytical.UI
+{
+    //public class PanelSection3D : ISection3DObject, ISegment3DObject
+    //{
+    //    private Panel Panel;
+    //    private double height;
+
+    //    public PanelSection3D(Panel panel, double height)
+    //        : base(panel)
+    //    {
+    //        this.height = height;
+    //    }
+
+    //    public Segment3D Segment3D => throw new NotImplementedException();
+    //}
+}
---
+++ WPF/SAM.Geometry.UI.WPF/Classes/UIGeometryObjectModel.cs
@@ -0,0 +1,23 @@
+﻿namespace SAM.Geometry.UI.WPF
+{
+    public class UIGeometryObjectModel : Core.UI.UIJSAMObject<GeometryObjectModel>
+    {
+        public UIGeometryObjectModel(string path)
+       : base(path)
+        {
+
+        }
+
+        public UIGeometryObjectModel(GeometryObjectModel geometryObjectModel)
+            : base(geometryObjectModel)
+        {
+
+        }
+
+        public UIGeometryObjectModel()
+            : base()
+        {
+
+        }
+    }
+}
