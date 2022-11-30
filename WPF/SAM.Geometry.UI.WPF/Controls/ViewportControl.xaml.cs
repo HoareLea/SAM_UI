f2da99f866e94564a2ea074877447dcf3419ba3c
Ziolkowski <jakubziolkowski@digiproject.uk>
2022-11-30 14:40:30 +0000

    Unnecessary Restore Removed

--- WPF/SAM.Geometry.UI.WPF/Controls/ViewportControl.xaml.cs
+++ WPF/SAM.Geometry.UI.WPF/Controls/ViewportControl.xaml.cs
@@ -92,9 +92,9 @@ namespace SAM.Geometry.UI.WPF
                     foreach (IVisualJSAMObject visualSAMObject_Selected in visualSAMObjects_Selected)
                     {
                         visualSAMObject_Selected.SetSelected(false);
-                        Modify.Restore((visualSAMObject_Selected as dynamic)?.Content);
+                        //Modify.Restore((visualSAMObject_Selected as dynamic)?.Content);
                         visualSAMObject_Selected.SetHighlight(false);
-                        Modify.Restore((visualSAMObject_Selected as dynamic)?.Content);
+                        //Modify.Restore((visualSAMObject_Selected as dynamic)?.Content);
                     }
                 }
 
@@ -105,7 +105,7 @@ namespace SAM.Geometry.UI.WPF
             if (highlight)
             {
                 visualSAMObject_Highlight?.SetHighlight(false);
-                Modify.Restore((visualSAMObject_Highlight as dynamic)?.Content);
+                //Modify.Restore((visualSAMObject_Highlight as dynamic)?.Content);
                 visualSAMObject_Highlight = null;
 
             }
@@ -197,7 +197,7 @@ namespace SAM.Geometry.UI.WPF
         private void helixViewport3D_PreviewMouseMove(object sender, MouseEventArgs e)
         {
             visualSAMObject_Highlight?.SetHighlight(false);
-            Modify.Restore((visualSAMObject_Highlight as dynamic)?.Content);
+            //Modify.Restore((visualSAMObject_Highlight as dynamic)?.Content);
             if(visualSAMObjects_Selected != null && visualSAMObjects_Selected.Count > 0)
             {
                 if(visualSAMObjects_Selected.Contains(visualSAMObject_Highlight))
@@ -258,7 +258,7 @@ namespace SAM.Geometry.UI.WPF
             if(visualSAMObjects_Selected.Contains(visualJSAMObject))
             {
                 visualJSAMObject.SetSelected(false);
-                Modify.Restore((visualJSAMObject as dynamic).Content);
+                //Modify.Restore((visualJSAMObject as dynamic).Content);
                 visualSAMObjects_Selected.Remove(visualJSAMObject);
             }
             else
