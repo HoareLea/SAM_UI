6be0f0b1ce8dbd52ef675d422584d209cf489b07
Ziolkowski <jakubziolkowski@digiproject.uk>
2023-01-16 11:46:10 +0000

    TM59Manager Implemented

--- WPF/SAM.Analytical.UI.WPF/Controls/MapTM59InternalConditionsControl.xaml.cs
+++ WPF/SAM.Analytical.UI.WPF/Controls/MapTM59InternalConditionsControl.xaml.cs
@@ -161,156 +161,17 @@ namespace SAM.Analytical.UI.WPF
             SetMapFunc();
         }
 
-        private static bool IsSleeping(Space space, TextMap textMap)
-        {
-            return Is(space, textMap, "Sleeping");
-        }
-
-        private static bool IsLiving(Space space, TextMap textMap)
-        {
-            return Is(space, textMap, "Living");
-        }
-
-        private static bool IsCooking(Space space, TextMap textMap)
-        {
-            return Is(space, textMap, "Cooking");
-        }
-
-        private enum Application
-        {
-            Sleeping,
-            Living,
-            Cooking
-        }
-
-        private static List<Application> Applications(Space space, TextMap textMap)
-        {
-            if(space == null || textMap == null)
-            {
-                return null;
-            }
-
-            List<Application> result = new List<Application>();
-            if(IsSleeping(space, textMap))
-            {
-                result.Add(Application.Sleeping);
-            }
-
-            if (IsLiving(space, textMap))
-            {
-                result.Add(Application.Living);
-            }
-
-            if (IsCooking(space, textMap))
-            {
-                result.Add(Application.Cooking);
-            }
-
-            return result;
-
-        }
-
-        private static bool Is(Space space, TextMap textMap, string key)
-        {
-            if (space == null || textMap == null || string.IsNullOrEmpty(key))
-            {
-                return false;
-            }
-
-            HashSet<string> values = textMap.GetSortedKeys(space.Name);
-            return values != null && values.Contains(key);
-        }
-
-        private static int Count(Space space, TextMap textMap)
-        {
-            if (space == null || textMap == null)
-            {
-                return 0;
-            }
-
-            HashSet<string> values = textMap.GetSortedKeys(space.Name);
-            foreach(string value in values)
-            {
-                if(Core.Query.TryConvert<int>(value, out int result))
-                {
-                    return result;
-                }
-            }
-
-            return 0;
-        }
-
         public void SetMapFunc()
         {
             TextMap textMap = TextMap;
             InternalConditionLibrary internalConditionLibrary = InternalConditionLibrary;
             string zoneType = comboBox_ZoneType.SelectedItem as string;
 
+            TM59Manager tM59Manager = new TM59Manager(textMap);
+
             mapInternalConditionsControl.MapFunc = new Func<Space, InternalCondition>(x => 
             {
-                Zone zone = adjacencyCluster?.GetZones(x, zoneType)?.FirstOrDefault();
-                if(zone == null)
-                {
-                    return null;
-                }
-
-                List<Space> spaces = adjacencyCluster.GetSpaces(zone);
-                if(spaces == null || spaces.Count == 0)
-                {
-                    return null;
-                }
-
-                List<Application> applications = Applications(x, textMap);
-                if (applications.Contains(Application.Sleeping) && applications.Contains(Application.Cooking) && applications.Contains(Application.Living))
-                {
-                    return internalConditionLibrary.GetInternalConditions("Studio").FirstOrDefault();
-                }
-
-                int count = 0;
-
-                if(applications.Contains(Application.Sleeping))
-                {
-                    count = Count(x, textMap);
-                    count = count == 0 ? 1 : count;
-
-                    switch(count)
-                    {
-                        case 1:
-                            return internalConditionLibrary.GetInternalConditions("Single Bedroom").FirstOrDefault();
-
-                        case 2:
-                            return internalConditionLibrary.GetInternalConditions("Double Bedroom").FirstOrDefault();
-                    }
-
-                    return null;
-                }
-
-                List<Space> spaces_Sleeping = spaces.FindAll(y => IsSleeping(y, textMap));
-
-                count = spaces_Sleeping.ConvertAll(y => Count(y, textMap)).Sum();
-                count = count == 0 ? 1 : count;
-                count = count > 3 ? 3 : count;
-
-                string name = null;
-                if (applications.Contains(Application.Cooking) && applications.Contains(Application.Living))
-                {
-                    name = "Bed Apt. Living Room/Kitchen";
-                }
-                else if(applications.Contains(Application.Cooking))
-                {
-                    name = "Bed Apt. Kitchen";
-                }
-                else if(applications.Contains(Application.Living))
-                {
-                    name = "Bed Apt. Living Room";
-                }
-                else
-                {
-                    return null;
-                }
-
-                name = string.Format("{0} {1}", count, name);
-                return internalConditionLibrary.GetInternalConditions(name).FirstOrDefault();
+                return tM59Manager.GetInternalCondition(adjacencyCluster, internalConditionLibrary, x, zoneType);
             });
         }
 
