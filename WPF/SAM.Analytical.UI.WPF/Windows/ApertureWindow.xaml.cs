4990a4eacdbe1765c2310a55a4b985d986ad83b9
Ziolkowski <jakubziolkowski@digiproject.uk>
2023-02-14 11:46:57 +0000

    ApertureWindow Updated (Discharge Coefficient Value Check Added)

--- WPF/SAM.Analytical.UI.WPF/Windows/ApertureWindow.xaml.cs
+++ WPF/SAM.Analytical.UI.WPF/Windows/ApertureWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Windows.Forms;
 
 namespace SAM.Analytical.UI.WPF
 {
@@ -34,6 +35,26 @@ namespace SAM.Analytical.UI.WPF
 
         private void button_OK_Click(object sender, System.Windows.RoutedEventArgs e)
         {
+            List<Aperture> apertures = Apertures;
+            if(apertures != null)
+            {
+                foreach (Aperture aperture in apertures)
+                {
+                    if(aperture == null || !aperture.TryGetValue(ApertureParameter.OpeningProperties, out IOpeningProperties openingProperies) || openingProperies == null)
+                    {
+                        continue;
+                    }
+
+                    double dischargeCoefficient = openingProperies.GetDischargeCoefficient();
+                    if(double.IsNaN(dischargeCoefficient) || dischargeCoefficient <= 0 || dischargeCoefficient > 1)
+                    {
+                        MessageBox.Show("Discharge coefficient value has to be greater than 0 and less than 1");
+                        return;
+                    }
+                }
+            }
+
+
             DialogResult = true;
 
             Close();
