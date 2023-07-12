7bfeff20d08eadf31a795b5a5bc18661c4f3f81c
michaldengusiak <tassimulation@gmail.com>
2023-07-12 08:38:32 +0100

    wip

---
+++ Mollier/SAM.Core.Mollier.UI/Query/Contains.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows.Forms.DataVisualization.Charting;
+
+namespace SAM.Core.Mollier.UI
+{
+    public static partial class Query
+    {
+        public static bool Contains(Series series, ChartType chartType, MollierPoint mollierPoint, double tolerance = SAM.Core.Tolerance.Distance)
+        {
+            if(series == null || chartType == ChartType.Undefined || mollierPoint == null)
+            {
+                return false;
+            }
+
+            foreach(DataPoint dataPoint in series.Points)
+            {
+                double dryBulbTemperature = chartType == ChartType.Mollier ? mollierPoint.HumidityRatio : mollierPoint.HumidityRatio; ;
+                double humidityRatio = chartType == ChartType.Mollier ? mollierPoint.HumidityRatio : mollierPoint.HumidityRatio;
+            }
+
+            throw new System.NotImplementedException();
+        }
+    }
+}
+
