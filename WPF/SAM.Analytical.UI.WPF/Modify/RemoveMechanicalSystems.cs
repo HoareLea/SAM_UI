cee8c99d25ec4206aaa6f0af06eca4855fda4850
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2023-10-03 12:55:25 +0200

    ContexMenu Updated with Remove Options

---
+++ WPF/SAM.Analytical.UI.WPF/Modify/RemoveMechanicalSystems.cs
@@ -0,0 +1,39 @@
+﻿
+using SAM.Core;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SAM.Analytical.UI.WPF
+{
+    public static partial class Modify
+    {
+        public static void RemoveMechanicalSystems<T>(this UIAnalyticalModel uIAnalyticalModel, IEnumerable<T> mechanicalSystems) where T: MechanicalSystem
+        {
+            AnalyticalModel analyticalModel = uIAnalyticalModel?.JSAMObject;
+            if (analyticalModel == null || mechanicalSystems == null || mechanicalSystems.Count() == 0)
+            {
+                return;
+            }
+
+            AdjacencyCluster adjacencyCluster = analyticalModel.AdjacencyCluster;
+            ProfileLibrary profileLibrary = analyticalModel.ProfileLibrary;
+
+            List<SAMObject> sAMObjects = new List<SAMObject>();
+            foreach (T mechanicalSystem in mechanicalSystems)
+            {
+                if (mechanicalSystem == null)
+                {
+                    continue;
+                }
+
+                bool removed = adjacencyCluster.RemoveObject(mechanicalSystem.GetType(), mechanicalSystem.Guid);
+                if (removed)
+                {
+                    sAMObjects.Add(mechanicalSystem);
+                }
+            }
+
+            uIAnalyticalModel.SetJSAMObject( new AnalyticalModel(analyticalModel, adjacencyCluster, analyticalModel.MaterialLibrary, profileLibrary), new AnalyticalModelModification(sAMObjects));
+        }
+    }
+}
---
+++ WPF/SAM.Analytical.UI.WPF/Modify/RemoveZones.cs
@@ -0,0 +1,39 @@
+﻿
+using SAM.Core;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SAM.Analytical.UI.WPF
+{
+    public static partial class Modify
+    {
+        public static void RemoveZones(this UIAnalyticalModel uIAnalyticalModel, IEnumerable<Zone> zones)
+        {
+            AnalyticalModel analyticalModel = uIAnalyticalModel?.JSAMObject;
+            if (analyticalModel == null || zones == null || zones.Count() == 0)
+            {
+                return;
+            }
+
+            AdjacencyCluster adjacencyCluster = analyticalModel.AdjacencyCluster;
+            ProfileLibrary profileLibrary = analyticalModel.ProfileLibrary;
+
+            List<SAMObject> sAMObjects = new List<SAMObject>();
+            foreach(Zone zone in zones)
+            {
+                if(zone == null)
+                {
+                    continue;
+                }
+
+                bool removed = adjacencyCluster.RemoveObject(zone.GetType(), zone.Guid);
+                if(removed)
+                {
+                    sAMObjects.Add(zone);
+                }
+            }
+
+            uIAnalyticalModel.SetJSAMObject( new AnalyticalModel(analyticalModel, adjacencyCluster, analyticalModel.MaterialLibrary, profileLibrary), new AnalyticalModelModification(sAMObjects));
+        }
+    }
+}
