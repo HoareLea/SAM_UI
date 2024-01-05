bb6e80346670aba23b3b57ed82300f04489466dd
michaldengusiak <tassimulation@gmail.com>
2024-01-05 12:55:31 +0000

    fix opening process

--- Grasshopper/SAM.Core.Mollier.UI.Grasshopper/Query/MollierObjects.cs
+++ Grasshopper/SAM.Core.Mollier.UI.Grasshopper/Query/MollierObjects.cs
@@ -1,5 +1,6 @@
 ﻿using Grasshopper.Kernel;
 using Grasshopper.Kernel.Data;
+using Grasshopper.Kernel.Types;
 using SAM.Core.Grasshopper.Mollier;
 using System.Collections.Generic;
 
@@ -17,16 +18,16 @@ namespace SAM.Core.Mollier.UI.Grasshopper
             List<IMollierObject> result = new List<IMollierObject>();
             foreach (IGH_Param gH_Param in gH_Params)
             {
-                GooMollierObjectParam gooMollierObjectParam = gH_Param as GooMollierObjectParam;
-                if (gooMollierObjectParam == null)
-                {
-                    continue;
-                }
+                //GooMollierObjectParam gooMollierObjectParam = gH_Param as GooMollierObjectParam;
+                //if (gooMollierObjectParam == null)
+                //{
+                //    continue;
+                //}
 
-                IGH_Structure gH_Structure = gooMollierObjectParam.VolatileData;
+                IGH_Structure gH_Structure = gH_Param.VolatileData;
                 foreach (object @object in gH_Structure.AllData(true))
                 {
-                    IMollierObject mollierObject = (@object as GooMollierObject)?.Value;
+                    IMollierObject mollierObject = @object is IGH_Goo ? (@object as dynamic).Value : @object as IMollierObject;
                     if (mollierObject != null)
                     {
                         result.Add(mollierObject);
