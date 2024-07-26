e3c94d7b1ba55864646f0c69e289f7023701982d
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2024-07-26 17:20:17 +0200

    Code Cleaning

--- Core/Grasshopper/SAM.Core.Mollier.UI.Grasshopper/Properties/AssemblyInfo.cs
+++ Core/Grasshopper/SAM.Core.Mollier.UI.Grasshopper/Properties/AssemblyInfo.cs
@@ -1,15 +1,5 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("SAM.Core.Mollier.UI.Grasshopper")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SAM.Core.Mollier.UI.Grasshopper")]
-[assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,16 +10,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("1218dfb7-b5a1-4c7c-b718-43677b18b7a8")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
