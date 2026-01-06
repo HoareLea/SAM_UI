905acc7dd21187f20c8e33dc1da4ec5fdb873343
Jakub Ziolkowski <16777757+ZiolkowskiJakub@users.noreply.github.com>
2026-01-06 10:01:18 +0000

    Add license and copyright to CreateCaseByWeatherDataControl
    Added SPDX license identifier and copyright notice.

--- WPF/SAM.Analytical.UI.WPF/Controls/CreateCaseByWeatherDataControl.xaml.cs
+++ WPF/SAM.Analytical.UI.WPF/Controls/CreateCaseByWeatherDataControl.xaml.cs
@@ -1,4 +1,7 @@
-﻿using SAM.Analytical.Classes;
+// SPDX-License-Identifier: LGPL-3.0-or-later
+// Copyright (c) 2020-2026 Michal Dengusiak & Jakub Ziolkowski and contributors
+
+using SAM.Analytical.Classes;
 using SAM.Core;
 using SAM.Core.UI.WPF;
 using SAM.Weather;
