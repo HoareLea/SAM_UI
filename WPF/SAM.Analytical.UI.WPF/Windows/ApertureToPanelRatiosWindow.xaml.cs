8083c1206daa049b62290a731622589262318da4
Jakub Ziolkowski <16777757+ZiolkowskiJakub@users.noreply.github.com>
2026-01-06 10:11:45 +0000

    Add license and copyright information
    Added SPDX license identifier and copyright notice.

--- WPF/SAM.Analytical.UI.WPF/Windows/ApertureToPanelRatiosWindow.xaml.cs
+++ WPF/SAM.Analytical.UI.WPF/Windows/ApertureToPanelRatiosWindow.xaml.cs
@@ -1,6 +1,8 @@
-﻿using SAM.Analytical.Classes;
-using System.Windows;
+// SPDX-License-Identifier: LGPL-3.0-or-later
+// Copyright (c) 2020-2026 Michal Dengusiak & Jakub Ziolkowski and contributors
 
+using SAM.Analytical.Classes;
+using System.Windows;
 
 namespace SAM.Analytical.UI.WPF
 {
