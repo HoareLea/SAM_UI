952f4c55479a1ac07d650d711b02d8a0eba62d99
Jakub Ziolkowski <16777757+ZiolkowskiJakub@users.noreply.github.com>
2026-01-06 10:03:36 +0000

    Add license and copyright comments to SimulateControl
    Added SPDX license identifier and copyright notice.

--- WPF/SAM.Analytical.UI.WPF/Controls/SimulateControl.xaml.cs
+++ WPF/SAM.Analytical.UI.WPF/Controls/SimulateControl.xaml.cs
@@ -1,4 +1,7 @@
-﻿using SAM.Core;
+// SPDX-License-Identifier: LGPL-3.0-or-later
+// Copyright (c) 2020-2026 Michal Dengusiak & Jakub Ziolkowski and contributors
+
+using SAM.Core;
 using SAM.Core.UI.WPF;
 using SAM.Weather;
 using System;
