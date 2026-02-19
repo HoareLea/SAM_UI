d13371e26d319658e91c139b1c4ed3ed894b1940
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2026-02-19 13:11:14 +0100

    ColorGradientWindow implemented

--- WPF/SAM.Core.UI.WPF/Controls/LegendControl.xaml.cs
+++ WPF/SAM.Core.UI.WPF/Controls/LegendControl.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -13,9 +14,7 @@ namespace SAM.Core.UI.WPF
     public partial class LegendControl : UserControl
     {
         private Legend legend;
-        public bool Sort { get; set; } = true;
-        public LegendItem UndefinedLegendItem { get; set; }
-
+        
         public LegendControl()
         {
             InitializeComponent();
@@ -38,6 +37,120 @@ namespace SAM.Core.UI.WPF
             {
                 SetLegend(value);
             }
+        }
+
+        public bool Sort { get; set; } = true;
+        
+        public LegendItem UndefinedLegendItem { get; set; }
+        
+        private int Add(LegendItem legendItem)
+        {
+            if (legendItem == null)
+            {
+                return -1;
+            }
+
+            StackPanel stackPanel = new StackPanel() { Orientation = Orientation.Horizontal, IsEnabled = legendItem.Editable };
+
+            string text = legendItem.Text != null && legendItem.Text.Contains("_") ? string.Format("_{0}", legendItem.Text) : legendItem.Text; //TODO: Temporary solution, first occurance of "_" symbol will be removed from Label Content??
+
+            TextBox textBox = new TextBox() { IsReadOnly = true, Width = 20, Background = new SolidColorBrush(legendItem.Color.ToMedia()), TextWrapping = TextWrapping.WrapWithOverflow };
+            stackPanel.Children.Add(textBox);
+
+            Label label = new Label() { Content = text };
+            stackPanel.Children.Add(label);
+
+            stackPanel.Tag = legendItem;
+
+            if (legendItem.Editable)
+            {
+                textBox.Cursor = Cursors.Hand;
+                label.Cursor = Cursors.Hand;
+
+                textBox.MouseDoubleClick += Control_DoubleClick;
+                label.MouseDoubleClick += Control_DoubleClick;
+            }
+
+            return this.stackPanel.Children.Add(stackPanel);
+        }
+
+        private void Button_GradientColor_Click(object sender, RoutedEventArgs e)
+        {
+            if(legend?.LegendItems is not List<LegendItem> legendItems || legendItems.Count < 2)
+            {
+                return;
+            }
+
+            ColorGradientWindow colorGradientWindow = new ColorGradientWindow();
+            colorGradientWindow.Low = legendItems.First().Color;
+            colorGradientWindow.High = legendItems.Last().Color;
+            if(legendItems.Count > 2)
+            {
+                colorGradientWindow.Mid = legendItems[legendItems.Count / 2].Color;
+                colorGradientWindow.HasMid = true;
+            }
+
+            bool? dialogResult = colorGradientWindow.ShowDialog();
+            if(dialogResult is null || !dialogResult.HasValue)
+            {
+                return;
+            }
+
+            System.Drawing.Color low = colorGradientWindow.Low;
+            System.Drawing.Color high = colorGradientWindow.High;
+            System.Drawing.Color mid = colorGradientWindow.HasMid ? colorGradientWindow.Mid : System.Drawing.Color.FromArgb((low.R + high.R)/2, (low.G + high.G) / 2, (low.B + high.B) / 2);
+
+            List<System.Drawing.Color> colors = ColorGradientInterpolationGenerator.GenerateDiverging(low, mid, high, legendItems.Count);
+            if(colors is null || colors.Count != legendItems.Count)
+            {
+                return;
+            }
+
+            for(int i =0; i < legendItems.Count; i++)
+            {
+                legendItems[i] = new LegendItem(colors[i], legendItems[i].Text);
+            }
+
+            Legend legend_New = new Legend(legend.Name, legendItems);
+
+            SetLegend(legend_New);
+        }
+
+        private void Control_DoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            UpdateColor((sender as Control).Parent as StackPanel);
+        }
+
+        private Legend GetLegend()
+        {
+            Legend result = legend == null ? new Legend(textBox_Name.Text) : new Legend(textBox_Name.Text, legend);
+
+            result.Visible = true;
+            if (checkBox_Visible.IsChecked != null && checkBox_Visible.IsChecked.HasValue)
+            {
+                result.Visible = checkBox_Visible.IsChecked.Value;
+            }
+            List<LegendItem> legendItems = new List<LegendItem>();
+            foreach (UIElement uIElement in stackPanel.Children)
+            {
+                StackPanel stackPanel_Temp = uIElement as StackPanel;
+                if (stackPanel_Temp == null)
+                {
+                    continue;
+                }
+
+                LegendItem legendItem = stackPanel_Temp.Tag as LegendItem;
+                if (legendItem == null)
+                {
+                    continue;
+                }
+
+                legendItems.Add(legendItem);
+            }
+
+            legendItems.ForEach(x => result.Update(x.Color, x.Text));
+
+            return result;
         }
 
         private void SetLegend(Legend legend)
@@ -106,75 +219,7 @@ namespace SAM.Core.UI.WPF
                 }
             }
         }
-
-        private Legend GetLegend()
-        {
-            Legend result = legend == null ? new Legend(textBox_Name.Text) : new Legend(textBox_Name.Text, legend);
-
-            result.Visible = true;
-            if (checkBox_Visible.IsChecked != null && checkBox_Visible.IsChecked.HasValue)
-            {
-                result.Visible = checkBox_Visible.IsChecked.Value;
-            }
-            List<LegendItem> legendItems = new List<LegendItem>();
-            foreach(UIElement uIElement in stackPanel.Children)
-            {
-                StackPanel stackPanel_Temp = uIElement as StackPanel;
-                if (stackPanel_Temp == null)
-                {
-                    continue;
-                }
-
-                LegendItem legendItem = stackPanel_Temp.Tag as LegendItem;
-                if(legendItem == null)
-                {
-                    continue;
-                }
-
-                legendItems.Add(legendItem);
-            }
-
-            legendItems.ForEach(x => result.Update(x.Color, x.Text));
-
-            return result;
-        }
-
-        private int Add(LegendItem legendItem)
-        {
-            if(legendItem == null)
-            {
-                return -1;
-            }
-
-            StackPanel stackPanel = new StackPanel() { Orientation = Orientation.Horizontal, IsEnabled = legendItem.Editable};
-
-            string text = legendItem.Text != null && legendItem.Text.Contains("_") ? string.Format("_{0}", legendItem.Text) : legendItem.Text; //TODO: Temporary solution, first occurance of "_" symbol will be removed from Label Content??
-
-            TextBox textBox = new TextBox() { IsReadOnly = true, Width = 20, Background = new SolidColorBrush(legendItem.Color.ToMedia()), TextWrapping = TextWrapping.WrapWithOverflow };
-            stackPanel.Children.Add(textBox);
-
-            Label label = new Label() { Content = text };
-            stackPanel.Children.Add(label);
-            
-            stackPanel.Tag = legendItem;
-
-            if(legendItem.Editable)
-            {
-                textBox.Cursor = Cursors.Hand;
-                label.Cursor = Cursors.Hand;
-
-                textBox.MouseDoubleClick += Control_DoubleClick;
-                label.MouseDoubleClick += Control_DoubleClick;
-            }
-
-            return this.stackPanel.Children.Add(stackPanel);
-        }
-
-        private void Control_DoubleClick(object sender, MouseButtonEventArgs e)
-        {
-            UpdateColor((sender as Control).Parent as StackPanel);
-        }
-
+        
         private void UpdateColor(StackPanel stackPanel)
         {
             if(stackPanel == null)
---
+++ WPF/SAM.Core.UI.WPF/Windows/ColorGradientWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SAM.Core.UI.WPF
+{
+    /// <summary>
+    /// Interaction logic for ColorGradientWindow.xaml
+    /// </summary>
+    public partial class ColorGradientWindow : Window
+    {
+        public ColorGradientWindow()
+        {
+            InitializeComponent();
+        }
+
+        public bool HasMid
+        {
+            get
+            {
+                return colorGradientControl_Main.HasMid;
+            }
+
+            set
+            {
+                colorGradientControl_Main.HasMid = value;
+            }
+        }
+
+        public System.Drawing.Color High
+        {
+            get
+            {
+                return colorGradientControl_Main.High;
+            }
+
+            set
+            {
+                colorGradientControl_Main.High = value;
+            }
+        }
+
+        public System.Drawing.Color Low
+        {
+            get
+            {
+                return colorGradientControl_Main.Low;
+            }
+
+            set
+            {
+                colorGradientControl_Main.Low = value;
+            }
+        }
+
+        public System.Drawing.Color Mid
+        {
+            get
+            {
+                return colorGradientControl_Main.Mid;
+            }
+
+            set
+            {
+                colorGradientControl_Main.Mid = value;
+            }
+        }
+
+        private void button_Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+
+        private void button_OK_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult= true;
+        }
+    }
+}
