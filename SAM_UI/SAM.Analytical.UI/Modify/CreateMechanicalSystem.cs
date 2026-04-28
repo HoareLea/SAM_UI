dc272b0834f322484bb707c3badba47191f7087e
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2026-04-28 12:34:13 +0200

    SPDX updated

--- SAM_UI/SAM.Analytical.UI/Modify/CreateMechanicalSystem.cs
+++ SAM_UI/SAM.Analytical.UI/Modify/CreateMechanicalSystem.cs
@@ -1,5 +1,5 @@
-﻿// SPDX - License - Identifier: LGPL - 3.0 - or - later
-// Copyright (c) 2020-2026 Michal Dengusiak & Jakub Ziolkowski and contributors
+﻿// SPDX-License-Identifier: LGPL-3.0-or-later
+// Copyright (c) 2020–2026 Michal Dengusiak & Jakub Ziolkowski and contributors
 
 using SAM.Core.Windows.Forms;
 using System.Collections.Generic;
